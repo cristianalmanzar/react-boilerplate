d9f2fefe76b8d9720d21f3379d22ef4598643426
Cristian Almanzar <cristian.almanzar@gmail.com>
2020-07-13 19:01:17 -0400

    Configuration for Github Pages

---
+++ docs/precache-manifest.364db162c85f3a60aacbb7b74ef95b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096b546d705352d762479a624c2dd2f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1982cc131f2dca61fc1d",
+    "url": "/static/css/main.fc004829.chunk.css"
+  },
+  {
+    "revision": "5ec0d3cd03b09b27b760",
+    "url": "/static/js/2.1f165f15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f165f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1982cc131f2dca61fc1d",
+    "url": "/static/js/main.3664dcf1.chunk.js"
+  },
+  {
+    "revision": "86e95e1bbae2bef5a117",
+    "url": "/static/js/runtime-main.94097570.js"
+  }
+]);
