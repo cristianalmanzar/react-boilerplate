47d03902cbf0bc8e2de5b51dd341c5634c910ca0
Cristian Almanzar <cristian.almanzar@gmail.com>
2020-07-27 15:55:54 -0400

    Updating GitHub pages !!

--- docs/precache-manifest.da2e588c3786a2dc021b3f37e53bd53c.js
+++ docs/precache-manifest.da2e588c3786a2dc021b3f37e53bd53c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "096b546d705352d762479a624c2dd2f3",
+    "revision": "f0273cce4f6ec87f189bfad6219e2d14",
     "url": "/index.html"
   },
   {
-    "revision": "1982cc131f2dca61fc1d",
-    "url": "/static/css/main.fc004829.chunk.css"
+    "revision": "4570095fcf9efe1d9840",
+    "url": "/static/css/main.0bec636f.chunk.css"
   },
   {
     "revision": "5ec0d3cd03b09b27b760",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1f165f15.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1982cc131f2dca61fc1d",
-    "url": "/static/js/main.3664dcf1.chunk.js"
+    "revision": "4570095fcf9efe1d9840",
+    "url": "/static/js/main.4c23bfe4.chunk.js"
   },
   {
     "revision": "86e95e1bbae2bef5a117",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.364db162c85f3a60aacbb7b74ef95b33.js"
+  "/precache-manifest.da2e588c3786a2dc021b3f37e53bd53c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
